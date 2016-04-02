0c92cf5f5072003a2a3d5e41123d1746a00f5fec
jennyhappy722 <u10316052@go.utaipei.edu.tw>
2016-04-02 17:33:55 +0800

    change size

--- ClockAnimation.java
+++ ClockAnimation.java
@@ -21,7 +21,7 @@ public class ClockAnimation extends Application {
 
     
     	// Create a scene and place it in the stage
-	Scene scene = new Scene(clock, 250, 250);
+	Scene scene = new Scene(clock, 700, 250);
     	primaryStage.setTitle("ClockAnimation"); // Set the stage title
     	primaryStage.setScene(scene); // Place the scene in the stage
     	primaryStage.show(); // Display the stage
